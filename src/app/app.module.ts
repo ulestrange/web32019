942e297abafa77b8d23ad377dfd9375ec8dd7f37
Una L'Estrange <lestrange.una@itsligo.ie>
2019-11-01 13:57:50 +0000

    Revert "Take in some sample components"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
-import { Sample1Component } from './sample1/sample1.component';
-import { Sample2Component } from './sample2/sample2.component';
-import { Sample3Component } from './sample3/sample3.component';
-import { HomeComponent } from './home/home.component';
-import { NotFoundComponent } from './core/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent,
-    Sample1Component,
-    Sample2Component,
-    Sample3Component,
-    HomeComponent,
-     NotFoundComponent
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
